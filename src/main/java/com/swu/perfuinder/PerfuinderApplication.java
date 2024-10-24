7a3eff4fced0d94f87dd7073f13d0fce4b8f930d
zwei1garden <dlwjddnjs081723@naver.com>
2024-10-24 20:06:26 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/swu/perfuinder/PerfuinderApplication.java
@@ -0,0 +1,13 @@
+package com.swu.perfuinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfuinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfuinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swu/perfuinder/PerfuinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swu.perfuinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfuinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
