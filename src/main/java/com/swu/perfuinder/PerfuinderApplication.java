d851e09768336893f11d4e7296179a2a03218443
zwei1garden <dlwjddnjs081723@naver.com>
2024-11-13 21:55:40 +0900

    [FEAT] #14 : war 배포를 위한 gradle 설정 및 의존성 추가

--- src/main/java/com/swu/perfuinder/PerfuinderApplication.java
+++ src/main/java/com/swu/perfuinder/PerfuinderApplication.java
@@ -2,11 +2,17 @@ package com.swu.perfuinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class PerfuinderApplication {
+public class PerfuinderApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PerfuinderApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerfuinderApplication.class, args);
